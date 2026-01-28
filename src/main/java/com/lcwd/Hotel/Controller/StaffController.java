8fa04692783e39a55f2a4db01a54cd02f54a59c9
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-29 00:36:47 +0530

    commit

---
+++ src/main/java/com/lcwd/Hotel/Controller/StaffController.java
@@ -0,0 +1,11 @@
+package com.lcwd.Hotel.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+
+}
