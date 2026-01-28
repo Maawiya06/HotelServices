3328b74fa287f8157f60dcd7f3915f540c282359
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-29 00:39:19 +0530

    commit

--- src/main/java/com/lcwd/Hotel/Controller/StaffController.java
+++ src/main/java/com/lcwd/Hotel/Controller/StaffController.java
@@ -1,11 +1,20 @@
 package com.lcwd.Hotel.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/staffs")
 public class StaffController {
 
-
+    public ResponseEntity<List<String>> getllstaff(){
+        List<String> list = Arrays.asList("Ram", "Radha", "Sheyam", "Krishna");
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
